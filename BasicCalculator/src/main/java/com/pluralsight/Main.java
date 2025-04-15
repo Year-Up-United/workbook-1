4dccd1afc1a019aa7aab74eda17d4dc3367784a4
thejaylajones <janeissaj@gmail.com>
2025-04-15 09:47:29 -0400

    Updated Payroll Calculator with overtime pay

--- BasicCalculator/src/main/java/com/pluralsight/Main.java
+++ BasicCalculator/src/main/java/com/pluralsight/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("What is the first number?");
---
+++ PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner payscanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name;
+        name = payscanner.nextLine();
+
+        System.out.println("How many hours did you work?");
+        double hourWage = payscanner.nextInt();
+
+        System.out.println("What is your pay rate?");
+        double hourpay = payscanner.nextInt();
+
+        System.out.printf("Hello %s, your gross pay is $%.0f",name, hourWage);
+
+
+
+
+    }
+}
