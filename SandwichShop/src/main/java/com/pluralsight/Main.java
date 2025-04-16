a4fd23662cd9c9f44cb6bc19427c94211dbb3ed3
thejaylajones <janeissaj@gmail.com>
2025-04-16 18:47:30 -0400

    Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/Main.java
+++ SandwichShop/src/main/java/com/pluralsight/Main.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Customer!");
+        System.out.println("Hello, Customer! Let's take your order!");
 
         // add scanner for input
         Scanner scanner = new Scanner(System.in);
