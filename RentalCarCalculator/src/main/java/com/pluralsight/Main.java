1d465ec4dbf8a6c2612c6e4aabbd55bc04220c1a
thejaylajones <janeissaj@gmail.com>
2025-04-17 09:31:38 -0400

    Completed Mod4 - Exercise 1 - Rental Car Calculator

--- RentalCarCalculator/src/main/java/com/pluralsight/Main.java
+++ RentalCarCalculator/src/main/java/com/pluralsight/Main.java
@@ -2,14 +2,14 @@ package com.pluralsight;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome, lets get you set up with a rental car!");
+        System.out.println("Welcome, let's get you set up with a rental car!");
 
 
 // add scanner for input
       Scanner scanner = new Scanner(System.in);
 
 // ask the first question
-      System.out.println("Please enter in your pickup day (mm/dd/yyyy) ");
+      System.out.println("Please enter in your pickup day (mm/dd/yyyy): ");
 // scanner for date they pick up car
       String rentalCar = scanner.nextLine();
 
@@ -51,16 +51,52 @@ public class Main {
       double tollTag = 3.95;
       double gpsDevice = 2.95;
       double roadAssit = 3.95;
-      double underageCharge = .30;
+      double underageChargeRate = .30;
 
 
+// if statements
+     if (age <=24){
+         System.out.println("You will be charged an additional 30% for your rental.");
+//         underageChargeRate = carRental - (carRental *.30);
+     }
 
+// compute prices
+    double basicRentalCost = carRental * numDays;
 
+// compute options
+    double optionsCost = 0;
 
+// if statements
+    if (tollPass.equalsIgnoreCase("yes")){
+        optionsCost += tollTag * numDays;
+    }
+    if (addGPS.equalsIgnoreCase("yes")){
+        optionsCost += gpsDevice * numDays;
+    }
+    if (roadHelp.equalsIgnoreCase("yes")){
+        optionsCost += roadAssit * numDays;
+    }
 
+// underage fee
+    double underageFee = 0;
 
+// underage calculation
+    if (age < 25){
+        underageFee = basicRentalCost * underageChargeRate;
+    }
 
+// total cost
+    double totalCost = basicRentalCost + optionsCost + underageFee;
 
+// print this at the end
+    System.out.printf("\nPickup Date: %s\n", rentalCar);
+    System.out.printf("Basic Rental Cost: $%.2f\n", basicRentalCost);
+    System.out.printf("Options Cost: $%.2f\n", optionsCost);
+    System.out.printf("Underage Driver additional fee: $%.2f\n", underageFee);
+    System.out.printf("Total Cost: $%.2f\n", totalCost);
+
+// thank you message
+    System.out.println("Thanks for choosing us to book your rental!");
 
 
 }
